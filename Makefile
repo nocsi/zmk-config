4f0462e79b2194e08258044ffdade422c8e965a2
Caleb White <cdwhite3@pm.me>
2023-09-22 10:11:04 -0500

    Clean up build process

--- Makefile
+++ Makefile
@@ -5,13 +5,14 @@ DOCKER := $(shell { command -v podman || command -v docker; })
 all:
 	$(DOCKER) build --tag zmk --file Dockerfile .
 	$(DOCKER) run --rm -it --name zmk \
-		-v $(PWD)/firmware:/app/firmware \
+        -v $(PWD)/build:/app/build \
 		-v $(PWD)/config:/app/config:ro \
+        -v $(PWD)/firmware:/app/firmware \
 		-e USERID=$(shell id -u) \
 		zmk
 
 clean:
-	rm -rf firmware/*.uf2
+	rm -rf firmware/*.uf2 build/[^.]*
 
 distclean:
 	$(DOCKER) image rm zmk docker.io/zmkfirmware/zmk-build-arm:stable
--- bin/build.sh
+++ bin/build.sh
@@ -2,16 +2,15 @@
 
 set -eu
 
-PWD=$(pwd)
+CONFIG_DIR="${CONFIG_DIR:-$(pwd)/config}"
 TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M%S")}"
 BUILD_MATRIX_PATH="${BUILD_MATRIX_PATH:-build.yaml}"
 USERID="${USERID:-$(id -u)}"
 
 while IFS=$',' read -r board shield; do
-    kebab_shield=${shield// /-}
     extra_cmake_args=${shield:+-DSHIELD="$shield"}
     display_name=${shield:+$shield - }${board}
-    artifact_name=${TIMESTAMP}-${kebab_shield:+$kebab_shield-}${board}-zmk
+    artifact_name=${shield:+${shield// /-}-}${board}-zmk
     build_dir="build/${artifact_name}"
 
     echo -e "\nEnvironment:"
@@ -24,13 +23,20 @@ while IFS=$',' read -r board shield; do
     echo -e "\n"
 
     echo -e "Building ${display_name}\n"
-    west build -s zmk/app -b "$board" -d ${build_dir} -- \
-        -DZMK_CONFIG="${PWD}/config" "${extra_cmake_args}"
 
-    echo -e "${display_name} KConfig\n"
-    grep -v -e "^#" -e "^$" ${build_dir}/zephyr/.config | sort
+    if [[ ! -d "$build_dir" ]]; then
+        west build -s zmk/app -b "$board" -d ${build_dir} -- \
+            -DZMK_CONFIG="$CONFIG_DIR" "${extra_cmake_args}"
+    else
+        west build -d ${build_dir}
+    fi
 
-    cp ${build_dir}/zephyr/zmk.uf2 "firmware/${artifact_name}.uf2"
+    # echo -e "${display_name} KConfig\n"
+    # grep -v -e "^#" -e "^$" ${build_dir}/zephyr/.config | sort
 
-    chown ${USERID} firmware/*
+    cp ${build_dir}/zephyr/zmk.uf2 "firmware/${TIMESTAMP}-${artifact_name}.uf2"
+
+    chown ${USERID} -R ${build_dir}
 done < <(yq '.include[] | [.board, .shield // ""] | @csv' ${BUILD_MATRIX_PATH})
+
+chown ${USERID} firmware/*
