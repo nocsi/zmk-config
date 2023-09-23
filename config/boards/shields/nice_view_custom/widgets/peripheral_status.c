23bd017227c190b3c7b15d39713b63936176fb70
Caleb White <cdwhite3@pm.me>
2023-09-23 15:36:20 -0500

    Randomize graphics

--- config/boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ config/boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -117,9 +117,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    // bool random = sys_rand32_get() & 1;
-    // lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_img_set_src(art, &neovim_flipped);
+    const lv_img_dsc_t* graphics[] = {&balloon, &mountain, &neovim_flipped, &neovim_icon};
+    lv_img_set_src(art, graphics[sys_rand32_get() % (sizeof(graphics) / sizeof(graphics[0]))]);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
