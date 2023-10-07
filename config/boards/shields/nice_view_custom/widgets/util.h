2a47dbf441c82716c473cee945356c133dce880d
Caleb White <cdwhite3@pm.me>
2023-10-07 06:54:27 -0500

    fix(nice_view): Fix incorrect union access

--- config/boards/shields/nice_view_custom/widgets/util.h
+++ config/boards/shields/nice_view_custom/widgets/util.h
@@ -20,9 +20,9 @@ struct status_state {
     bool charging;
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     struct zmk_endpoint_instance selected_endpoint;
+    int active_profile_index;
     bool active_profile_connected;
     bool active_profile_bonded;
-    uint8_t active_profile_index;
     uint8_t layer_index;
     const char *layer_label;
     uint8_t wpm[10];
